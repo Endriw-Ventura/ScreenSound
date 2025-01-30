166de1105ae215e25cd5e3d89344346a1f83ffac
Endriw Ventura <endriwventura@yahoo.com>
2025-01-30 17:01:47 -0300

    criação das extensions

--- ScreenSoundAPI/Endpoints/ArtistasExtensions.cs
+++ ScreenSoundAPI/Endpoints/ArtistasExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using ScreenSound.DataBase;
-using ScreenSound.Modelos;
-using System.Runtime.CompilerServices;
+using Microsoft.IdentityModel.Tokens;
+using ScreenSound.Shared.Data.DataBase;
+using ScreenSound.Shared.Models.Models;
+using ScreenSoundAPI.Requests;
+using ScreenSoundAPI.Responses;
 
 namespace ScreenSoundAPI.Endpoints
 {
@@ -11,7 +13,8 @@ namespace ScreenSoundAPI.Endpoints
         {
             app.MapGet("/Artistas", (Repository<Artista> db) =>
             {
-                return Results.Ok(db.GetAll());
+                var artistas = db.GetAll();
+                return Results.Ok(EntityListToReponseList(artistas));
             });
 
             app.MapGet("/Artistas/{nome}", (Repository<Artista> db, string nome) => {
@@ -19,24 +22,32 @@ namespace ScreenSoundAPI.Endpoints
                 if (artista is null)
                     return Results.NotFound();
 
-                return Results.Ok(artista);
+                return Results.Ok(new ArtistasResponse(artista.Id, artista.Nome, artista.FotoPerfil, artista.Bio));
             });
 
-            app.MapPost("/Artistas", (Repository<Artista> db, [FromBody] Artista artista) =>
+            app.MapPost("/Artistas", (Repository<Artista> db, [FromBody] ArtistasRequest artistaRequest) =>
             {
+                var artista = new Artista(artistaRequest.Nome, artistaRequest.Bio);
+                db.Add(artista);
                 return Results.Ok(artista.Id);
             });
 
-            app.MapPut("/Artistas", (Repository<Artista> db, [FromBody] Artista artista) => {
+            app.MapPut("/Artistas", (Repository<Artista> db, [FromBody] ArtistaRequestEdit artista) => {
                 var artistaAntigo = db.Get(a => a.Id == artista.Id);
                 if (artistaAntigo is null)
                     return Results.NotFound();
 
-                artistaAntigo.Nome = artista.Nome;
-                artistaAntigo.FotoPerfil = artista.FotoPerfil;
-                artistaAntigo.Bio = artista.Bio;
+                if (!artista.Nome.IsNullOrEmpty())
+                    artistaAntigo.Nome = artista.Nome;
+
+                if(!artista.FotoPerfil.IsNullOrEmpty())
+                    artistaAntigo.FotoPerfil = artista.FotoPerfil;
+
+                if(!artista.Bio.IsNullOrEmpty())
+                    artistaAntigo.Bio = artista.Bio;
+
                 db.Update(artistaAntigo);
-                return Results.Ok(artistaAntigo);
+                return Results.Ok();
             });
 
             app.MapDelete("/Artistas/{id}", (Repository<Artista> db, int id) =>
@@ -49,6 +60,20 @@ namespace ScreenSoundAPI.Endpoints
                 return Results.NoContent();
             });
 
+        }
+
+        private static ICollection<ArtistasResponse> EntityListToReponseList(IEnumerable<Artista> artistas)
+        {
+            return artistas.Select(a => EntityToResponse(a)).ToList();
+        }
+
+        private static ArtistasResponse EntityToResponse(Artista artista)
+        {
+            return new ArtistasResponse(
+                artista.Id,
+                artista.Nome, 
+                artista.FotoPerfil, 
+                artista.Bio);
         }
     }
 }
--- ScreenSoundAPI/Endpoints/MusicasExtensions.cs
+++ ScreenSoundAPI/Endpoints/MusicasExtensions.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using ScreenSound.DataBase;
-using ScreenSound.Modelos;
+using Microsoft.IdentityModel.Tokens;
+using ScreenSound.Shared.Data.DataBase;
+using ScreenSound.Shared.Models.Models;
+using ScreenSoundAPI.DTOs;
+using ScreenSoundAPI.Requests;
+using ScreenSoundAPI.Responses;
 
 namespace ScreenSoundAPI.Endpoints
 {
@@ -11,29 +15,38 @@ namespace ScreenSoundAPI.Endpoints
 
             app.MapGet("/Musicas", (Repository<Musica> db) =>
             {
-                return Results.Ok(db.GetAll());
+                var musicas = db.GetAll();
+                return Results.Ok(EntityListToReponseList(musicas));
             });
 
-            app.MapGet("/Musicas/{nome}", (Repository<Musica> db, string nome) => {
+            app.MapGet("/Musicas/{nome}", (Repository<Musica> db, string nome) =>
+            {
                 var musica = db.Get(a => a.Nome.ToLower().Equals(nome.ToLower()));
                 if (musica is null)
                     return Results.NotFound();
 
-                return Results.Ok(musica);
+                return Results.Ok(EntityToResponse(musica));
             });
 
-            app.MapPost("/Musicas", (Repository<Musica> db, [FromBody] Musica musica) =>
+            app.MapPost("/Musicas", (Repository<Musica> db, [FromBody] MusicasRequest musica) =>
             {
-                return Results.Ok(musica.Id);
+                var musicaNova = new Musica(musica.Nome);
+                db.Add(musicaNova);
+                return Results.Ok(musicaNova.Id);
             });
 
-            app.MapPut("/Musicas", (Repository<Musica> db, [FromBody] Musica musica) => {
+            app.MapPut("/Musicas", (Repository<Musica> db, [FromBody] MusicasRequestEdit musica) =>
+            {
                 var musicaAntiga = db.Get(a => a.Id == musica.Id);
                 if (musicaAntiga is null)
                     return Results.NotFound();
 
-                musicaAntiga.Nome = musica.Nome;
-                musicaAntiga.AnoLancamento = musica.AnoLancamento;
+                if (!musica.Nome.IsNullOrEmpty())
+                    musicaAntiga.Nome = musica.Nome;
+
+                if (!musica.AnoLancamento.IsNullOrEmpty())
+                    musicaAntiga.AnoLancamento = Convert.ToInt32(musica.AnoLancamento);
+
                 db.Update(musicaAntiga);
                 return Results.Ok(musicaAntiga);
             });
@@ -41,13 +54,26 @@ namespace ScreenSoundAPI.Endpoints
             app.MapDelete("/Musicas/{id}", (Repository<Musica> db, int id) =>
             {
                 var musica = db.Get(a => a.Id == id);
+
                 if (musica is null)
                     return Results.NotFound();
 
                 db.Delete(musica);
                 return Results.NoContent();
             });
+        }
+        private static ICollection<MusicasResponse> EntityListToReponseList(IEnumerable<Musica> artistas)
+        {
+            return artistas.Select(a => EntityToResponse(a)).ToList();
+        }
 
+        private static MusicasResponse EntityToResponse(Musica musica)
+        {
+            return new MusicasResponse(musica.Id,
+                musica.Artista?.Id, 
+                musica.Nome, 
+                musica.Artista?.Nome ?? "Artista Desconhecido",
+                musica.AnoLancamento);
         }
     }
 }
