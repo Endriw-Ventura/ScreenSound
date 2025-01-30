4c0d25cee555d816b890e32c883baa51416fc74c
Endriw Ventura <endriwventura@yahoo.com>
2025-01-30 17:03:40 -0300

    criação do relacionamento entre musica e genero

--- ScreenSound.Shared.Models/Models/Genero.cs
+++ ScreenSound.Shared.Models/Models/Genero.cs
@@ -11,5 +11,6 @@ namespace ScreenSound.Shared.Models.Models
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
+        public ICollection<Musica> Musicas { get; set; }
     }
 }
