31248d616b675a9d7407b932684f047ecd6d2db1
Endriw Ventura <endriwventura@yahoo.com>
2025-01-30 17:02:32 -0300

    criação do model genero

---
+++ ScreenSound.Shared.Models/Models/Genero.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Shared.Models.Models
+{
+    public class Genero
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+    }
+}
