be85ae325875d873cf242750a11055e40e9952d8
Endriw Ventura <endriwventura@yahoo.com>
2025-02-01 23:05:49 -0300

    migrations para atualizar o banco com as duas novas classes de autenticação

--- ScreenSound.Shared.Data/Migrations/20250202011609_CriacaoTabelasAuth.Designer.cs
+++ ScreenSound.Shared.Data/Migrations/20250202011609_CriacaoTabelasAuth.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ScreenSound.Shared.Data.DataBase;
 
@@ -11,9 +12,11 @@ using ScreenSound.Shared.Data.DataBase;
 namespace ScreenSound.Migrations
 {
     [DbContext(typeof(ScreenSoundContext))]
-    partial class ScreenSoundContextModelSnapshot : ModelSnapshot
+    [Migration("20250202011609_CriacaoTabelasAuth")]
+    partial class CriacaoTabelasAuth
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
